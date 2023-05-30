1c380e72e0c47d4d11adf5786ef2e6277c83d175
DuBrowski <krdubray@comcast.net>
2023-05-30 18:35:06 -0500

    better validation and function standardization

--- src/private/creationForm.php
+++ src/private/creationForm.php
@@ -53,7 +53,8 @@ if (!isset($_SESSION['loggedin'])) {
         <button type="button" id="add-question" class="btn btn-question">Add Question</button>
         <div class="survey-preview" id="preview"></div>
 
-        <button type="submit" id="submit-survey" class="btn btn-submit" style="display: none;">Submit Survey</button>
+        <button type="submit" onclick="return validate()" id="submit-survey" class="btn btn-submit"
+          style="display: none;">Submit Survey</button>
       </form>
       <?php
       if (isset($_GET["error"])) {
@@ -69,32 +70,51 @@ if (!isset($_SESSION['loggedin'])) {
     </div>
   </div>
   <script>
+
+    // validate form submission
+    function validate() {
+      if (document.getElementById('survey-name').value === "") {
+        console.log('no survey name provided');
+        return false;
+      };
+      return true;
+    }
+
     $(document).ready(function () {
       var questionCount = 1;
       var questionType;
+
 
       // Switch Question Type
       $('#question-type').on('change', function () {
         questionType = $(this).val();
         if (questionType == 'multiple_choice') {
-          $('#options-group').show();
-          while ($('#options-container').children().length != 2) {
-            if ($('#options-container').children().length < 2) {
-              addOption();
-            } else removeOption();
-          }
-          console.log(typeof $('#options-container').children().length);
-
+          optionSet();
         } else {
+          // if not multiple choice, must be text, remove all options and hide them
           questionType = 'text';
           while ($('#options-container').children().length > 0) removeOption();
           $('#options-group').hide();
         }
       });
 
+      // Makes option inputs visible, clears them, and sets to 2 inputs (the minimum)
+      function optionSet() {
+        $('#options-group').show();
+        $('.option-group input').each(function () {
+          $(this).val("");
+        });
+        while ($('#options-container').children().length != 2) {
+          if ($('#options-container').children().length < 2) {
+            addOption();
+          } else removeOption();
+        }
+      }
+
       // Add option to question
       $('#add-option').on('click', addOption);
 
+      // adds single option input HTML
       function addOption() {
         var optionCount = $('#options-container').children().length + 1;
         var optionHtml = '<div class="form-group option-group">';
@@ -107,9 +127,9 @@ if (!isset($_SESSION['loggedin'])) {
         }
       }
 
-      // Remove option from question
       $('#remove-option').on('click', removeOption);
 
+      // removes single option input HTML
       function removeOption() {
         $('#options-container').children().last().remove();
         if ($('#options-container').children().length == 2) {
@@ -119,36 +139,20 @@ if (!isset($_SESSION['loggedin'])) {
 
       // Add question to survey
       $('#add-question').on('click', function () {
-        var questionText = $('#question-text').val();
 
-        // ERROR- no question text
-        if (!questionText) {
-          $('#question-add-error').html("The question must have text.");
-          return;
-        }
-        var type = $('#question-type').val();
-        // ERROR- each option must have text
-        console.log(type);
-        let isOptionError = false;
-        if (type == 'multiple_choice') {
-          $('#options-container').children().each(function () {
-            if (!$(this).children('input').val()) {
-              isOptionError = true;
-            }
-          });
-        }
-        if (isOptionError) {
-          $('#options-error').html("Each option must have text");
-          return;
-        }
+        let questionText = $('#question-text').val();
+        let questionType = $('#question-type').val();
+
+        // Quit if error with question attmepting to add
+        if (isQuestionError(questionText, questionType)) return;
 
         var questionHtml = '<div class="form-group question-group">';
         questionHtml += '<h2>Question ' + questionCount + ": " + questionText + '</h2>';
-        questionHtml += '<input type="hidden" name="question_type[]" value="' + type + '">';
+        questionHtml += '<input type="hidden" name="question_type[]" value="' + questionType + '">';
         questionHtml += '<input type="hidden" name="question_text[]" value="' + questionText + '">';
         var optionsHtml = '';
         // for multiple choice questions, output question and each option
-        if ($('#question-type').val() == 'multiple_choice') {
+        if (questionType == 'multiple_choice') {
           var optionIndex = 1;
           $('.option-group input').each(function () {
             var optionLetter = String.fromCharCode(optionIndex + 64);
@@ -159,10 +163,14 @@ if (!isset($_SESSION['loggedin'])) {
             optionsHtml += '</div></div>';
             optionIndex++;
           });
+
+          // reset options
+          optionSet();
+
           optionsHtml += '<input type="hidden" name="options_count[]" value="' + (optionIndex - 1) + '">';
         }
         // only output question if text type question (free response)
-        else if ($('#question-type').val() == 'text') {
+        else if (questionType == 'text') {
           optionsHtml += '<input type="hidden" name="question_options[]" value="NULL">';
           optionsHtml += '<input type="hidden" name="options_count[]" value="1">';
           questionHtml += '</div>';
@@ -173,9 +181,35 @@ if (!isset($_SESSION['loggedin'])) {
         $('#submit-survey').show();
         questionCount++;
         $('#question-text').val('');
-        $('#survey-name').prop('readonly', true);
       });
+
+      // Returns TRUE if error with question addition.
+      // Also removes error HTML if no error present.
+      function isQuestionError(questionText, questionType) {
+
+        // ERROR- each question must have text
+        if (!questionText) {
+          $('#question-add-error').html("The question must have text.");
+          return true;
+        } else $('#question-add-error').html("");
+        // ERROR- each option must have text if multiple choice
+        let isOptionError = false;
+        if (questionType == 'multiple_choice') {
+          $('#options-container').children().each(function () {
+            if (!$(this).children('input').val()) {
+              isOptionError = true;
+            }
+          });
+        }
+        if (isOptionError) {
+          $('#options-error').html("Each option must have text");
+          return true;
+        } else $('#options-error').html("");
+      }
+      return false;
     });
+
+
   </script>
 </body>
 
